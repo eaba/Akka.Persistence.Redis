b266c56ea8171df72cc6e1b4061ecfde373eb56c
eabanonu <eabanonu@yahoo.com>
2020-12-17 13:26:38 +0100

    Update Akka version

--- src/Akka.Persistence.Redis.Tests/Query/RedisCurrentPersistenceIdsSpec.cs
+++ src/Akka.Persistence.Redis.Tests/Query/RedisCurrentPersistenceIdsSpec.cs
@@ -39,7 +39,7 @@ namespace Akka.Persistence.Redis.Tests.Query
         {
             ReadJournal = Sys.ReadJournalFor<RedisReadJournal>(RedisReadJournal.Identifier);
         }
-         
+        
         protected override void Dispose(bool disposing)
         {
             DbUtils.Clean(Database);
